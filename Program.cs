2cc8b58d350cb3edc33810217daa07307f98ad04
pluto <2274749769@qq.com>
2019-01-19 16:02:15 +0800

    01.19

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZDCore;
+
+namespace ZDWindowsFormsTest
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+           
+            ZDNetworkSerializable.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Welcome());
+        }
+    }
+}
